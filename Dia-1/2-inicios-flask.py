08ebbe53d8f2da534ca8a0dc1857ca6f27e05ea4
Rodrigo Arenas <jarenasl@pucp.edu.pe>
2023-02-07 23:40:17 -0500

    Se termino el dia01

--- Dia-1/2-inicios-flask.py
+++ Dia-1/2-inicios-flask.py
@@ -11,6 +11,15 @@ def inicio():
     return 'Hola desde mi servidor de Flaskkkkkk'
 
 
+@app.route('/mostrar-hora', methods=['GET', 'POST'])
+def mostrarHora():
+    # no se suele retornar strings (cadena de texto) sino que se utiliza diccionarios
+    return {
+        'content': '22:50:15'
+    }
+
+
+
 
 # debug > cada vez que modifiquemos algun archivo del proyecto y guardamos,, se reiniciara el servidor
 app.run(debug=True)
