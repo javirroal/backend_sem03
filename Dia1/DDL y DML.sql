c80f8880d21d1cf27df895dd7129b702d07936ad
Rodrigo Arenas <jarenasl@pucp.edu.pe>
2023-02-18 23:39:55 -0500

    DDL y DML

--- Dia1/DDL y DML.sql
+++ Dia1/DDL y DML.sql
@@ -62,7 +62,48 @@ SELECT * FROM usuarios WHERE nombre= 'Juana' AND id=2;
 -- Visualizar todas las tareas del usuario 1 y/o del usuario 2
 SELECT * FROM tareas WHERE usuario_id=1 OR usuario_id=2;
 
-INSERT INTO tareas (id, itulo, descripcion, usuario_id) VALUES (DEFAULT, 'Ir a comer', 'Comer pollo a la brasa', 1),
+INSERT INTO tareas (id, titulo, descripcion, usuario_id) VALUES (DEFAULT, 'Ir a comer', 'Comer pollo a la brasa', 1),
 																(DEFAULT, 'Comer pizza', 'Comer una rica pizza', 1);
 
-SELECT * FROM tareas WHERE usuario_id=1;
+-- buscar una palabra o palabras dentro de un texto
+SELECT * FROM tareas WHERE usuario_id=1 AND titulo LIKE '%comer%';
+SELECT * FROM usuarios WHERE nombre LIKE '%O';
+SELECT * FROM usuarios WHERE nombre LIKE 'J%';
+-- Si queremos hacer la distincion entre mayusculas y mimnusculas antes de poner el texto colocaremos la palabra BINARY y esto sirve para que haga la comparacion a nivel de numeros de caracteres (formato ASCII)
+SELECT * FROM usuarios WHERE nombre LIKE BINARY 'j%';
+
+-- El _ indica cuantos caracteres debe de 'saltar' para que busque el caracter indicado
+SELECT * FROM usuarios WHERE nombre LIKE '_u%';
+
+SELECT * FROM usuarios WHERE nombre NOT LIKE '_u%';
+
+INSERT INTO tareas (id, titulo, descripcion, usuario_id) VALUES (default, 'no hacer nada', 'no tengo explicacion', null);
+
+SELECT * FROM tareas;
+
+INSERT INTO tareas (id, titulo, descripcion, usuario_id) VALUES (DEFAULT, 'Jugar LOL', 'Jugar con mis amigos', 3);
+
+-- Interseccion entre la tabla usuarios con la tabla tareas 
+SELECT * FROM usuarios INNER JOIN tareas ON usuarios.id = tareas.usuario_id;
+
+SELECT * FROM usuarios LEFT JOIN tareas ON usuarios.id = tareas.usuario_id;
+
+SELECT * FROM usuarios RIGHT JOIN tareas ON usuarios.id = tareas.usuario_id;
+
+-- FULL OUTER JOIN (DEVUELVE TODO EL CONTENIDO)
+SELECT * FROM usuarios LEFT JOIN tareas ON usuarios.id = tareas.usuario_id UNION
+SELECT * FROM usuarios RIGHT JOIN tareas ON usuarios.id = tareas.usuario_id;
+
+-- UNION mezcla o combina las dos o mas consultas en una sola 'tabla virtual' pero estas consultas tienen que tener el mismo nimero de columnas, sino la union sera incorrecta
+SELECT id FROM usuarios UNION
+SELECT titulo FROM tareas;
+
+-- CONCATENAR -> juntar combinar, asi mismo si uso AS es para poner un alias o nombre a la columna
+SELECT CONCAT(titulo, ' ', descripcion) AS 'Nombre completo' FROM tareas;
+
+-- 1. Devolver todos los usuarios cuyo DNI contengan el numero 5
+SELECT * FROM usuarios WHERE dni LIKE '%5%';
+-- 2. Devolver todos los usuarios cuyo DNI tengan el tercer digito 8
+SELECT * FROM usuarios WHERE dni LIKE '__8%';
+-- 3. Devolver todas las tareas del usuario 'Javier'
+SELECT * FROM usuarios INNER JOIN tareas ON usuarios.id = tareas.usuario_id WHERE nombre ='Javier'; 
